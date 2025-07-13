27dd61f554f4c2860f222c2b7310ade06722498d
Otavio714 <jaootavio626@gmail.com>
2025-07-13 04:02:22 -0300

    init project

---
+++ script.js
@@ -0,0 +1,36 @@
+const bars = document.querySelector('.bars');
+const menu = document.querySelector('.menu');
+
+bars.addEventListener('click', () => {
+    console.log('Cliquei no menu!');
+    menu.classList.toggle('show_menu');
+});
+
+//OUR INSTITUTE SLIDERS
+
+var swiper = new Swiper(".instituteSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+    autoplay: {
+        delay: 1500,
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 6,
+
+        },
+        900: {
+            slidesPerView: 4,
+        },
+        500: {
+            slidesPerView: 2,
+        },
+        400: {
+            slidesPerView: 2,
+        },
+        375:{
+            slidesPerView:1
+        }
+    }
+});
